e307b89fc558736a17e1ff0420d8a3ab0c5ca8c7
SvetlanaDShopova <92042506+SvetlanaDShopova@users.noreply.github.com>
2023-05-04 12:46:54 -0400

    Big Decimal

---
+++ src/main/java/x_primality/Solution.java
@@ -0,0 +1,29 @@
+package x_primality;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Random;
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = bufferedReader.readLine();
+
+        BigInteger number = new BigInteger(n);
+
+        if(number.isProbablePrime(95)){
+            System.out.println("prime");
+        } else {
+            System.out.println("not prime");
+        }
+
+        bufferedReader.close();
+    }
+
+
+}
